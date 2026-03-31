94c011515b06ba8b1dc33c7e791f8e0b3a927425
Dumebi Johnson <ozordumebi@gmail.com>
2026-03-31 16:15:46 +0100

    Checkpoint from VS Code for cloud agent session

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getorCreateSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getorCreateAnvilEthConfig();
+        }
+    }
+
+    function getorCreateSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaEthPriceFeed =
+            NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return sepoliaEthPriceFeed;
+    }
+
+    function getorCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        // If we want to deploy to a local anvil chain, we want to deploy mocks!
+        // So instead of returning the real price feed address, we will deploy mocks when we are on anvil
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilEthPriceFeed = NetworkConfig({priceFeed: address(mockPriceFeed)});
+        return anvilEthPriceFeed;
+    }
+
+    // 1. Deploy a Mock Price Feed
+    // 2. Return the mock's address
+
+    //return activeNetworkConfig;
+    //}
+}
---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+    uint256 constant SEND_VALUE = 1e18;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    address USER = makeAddr("user");
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+
+    function setUp() external {
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployer = new DeployFundMe();
+        fundMe = deployer.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testDemo() public view {
+        // console.log("Hello World!");
+        // console.log("The number is: ", number);
+
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwner() public view {
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersion() public view {
+        //
+        assertEq(fundMe.getVersion(), 4);
+        // assertEq(fundMe.s_priceFeed().version(), 4);
+    }
+
+    function testIfFundingFails() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdateFundedDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: STARTING_BALANCE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, STARTING_BALANCE);
+    }
+
+    // function testGetFunder() public {
+    //     fundMe.fund{value: 1e18}();
+    //     address funder = fundMe.s_funders(0);
+    //     assertEq(funder, address(this));
+    //}
+
+    function testAddsFunderToArrayOfFunders() public funded {
+        vm.prank(USER);
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        uint256 startingOwnerBalance = fundMe.i_owner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        vm.prank(fundMe.i_owner());
+        fundMe.withdraw();
+
+        uint256 endingOwnerBalance = fundMe.i_owner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+    }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        //Arrange
+        uint160 numberOfFunders = 10;
+
+        for (uint160 i = 1; i <= numberOfFunders; i++) { // Start i from 1 for better labeling
+            address funderAddress = makeAddr(string.concat("funder", vm.toString(i)));
+            hoax(funderAddress, SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+            console.log("Funder %s funded with %s ETH", funderAddress, SEND_VALUE);
+        }
+
+        uint256 startingOwnerBalance = fundMe.i_owner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //Act
+        vm.startPrank(fundMe.i_owner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        uint256 endingOwnerBalance = fundMe.i_owner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+
+        //Assert
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+    }
+}
